be9e0e2aed3fcb56359634c40ff2a893c5138556
AwesomeAi <54154904+AwesomeAi@users.noreply.github.com>
2021-04-30 21:36:03 -0500

    Testing and building ready
    Test room for my code. Place holder objects in place holder folder. Don't touch. Ignore the memes.

---
+++ 2Determined/Assets/Scenes/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+
+    public void loadLevel(string level)
+    {
+        Debug.Log("attempting to load" + level + "...");
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("attemting to quit ...");
+        Application.Quit();
+    }
+}

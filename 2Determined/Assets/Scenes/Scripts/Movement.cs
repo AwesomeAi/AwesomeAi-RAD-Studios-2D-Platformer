4b68471cd720121eddb2dc18d583b31275792ed0
AwesomeAi <54154904+AwesomeAi@users.noreply.github.com>
2021-04-30 23:56:27 -0500

    Movement added
    Yay! closer to completion.

--- 2Determined/Assets/Scenes/Scripts/Movement.cs
+++ 2Determined/Assets/Scenes/Scripts/Movement.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float acceleration;
+    public float maxSpd;
+
+    float moveSpd = 0;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 current;
+
+        //Moves character left
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            //accelerates the character
+            if (this.moveSpd < this.maxSpd)
+            {
+                this.moveSpd += acceleration;
+
+                // puts character at the intended speed
+                if (this.moveSpd > this.maxSpd)
+                {
+                    this.moveSpd = this.maxSpd;
+                }
+            }
+
+            current = new Vector3(this.transform.position.x + this.moveSpd, this.transform.position.y, this.transform.position.z);
+
+            this.transform.position = current;
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //accelerates the character
+            if (this.moveSpd > -(this.maxSpd))
+            {
+                this.moveSpd -= acceleration;
+
+                // puts character at the intended speed
+                if (this.moveSpd < -(this.maxSpd))
+                {
+                    this.moveSpd = (-this.maxSpd);
+                }
+            }
+
+            current = new Vector3(this.transform.position.x + this.moveSpd, this.transform.position.y, this.transform.position.z);
+
+            this.transform.position = current;
+        }
+
+        else
+        {
+            //accelerates the character
+            if (this.moveSpd > 0)
+            {
+                this.moveSpd -= acceleration;
+
+                // puts character at the intended speed
+                if (this.moveSpd < 0)
+                {
+                    this.moveSpd = 0;
+                }
+            }
+
+            this.transform.position.Set(this.transform.position.x + this.moveSpd, this.transform.position.y, this.transform.position.z);
+        }
+
+
+
     }
+
+
 }

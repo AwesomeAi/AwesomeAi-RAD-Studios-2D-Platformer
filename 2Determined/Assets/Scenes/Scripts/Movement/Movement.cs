31c4f22f90d08115d5fd0d957d7d91927f4e14f5
AwesomeAi <54154904+AwesomeAi@users.noreply.github.com>
2021-05-06 23:36:34 -0500

    Added "proper" jumping
    jumps now require the character to be grounded

---
+++ 2Determined/Assets/Scenes/Scripts/Movement/Movement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private LayerMask ground; //checks for anything in the ground layer
+    [SerializeField] private LayerMask wall; //checks for walls
+
+    public float jumpVeloctiy;
+    public float moveSpd;
+
+    public float airMobility;
+
+
+    private Rigidbody2D rb2d;
+    private BoxCollider2D bc2d;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb2d = transform.GetComponent<Rigidbody2D>();
+        bc2d = transform.GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded()) // checks if player is grounded
+        {
+            jumping();
+        }
+
+        horizontal();
+    }
+
+
+    //runs jump code
+    private void jumping()
+    {
+        //Checks for keys
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.velocity = Vector2.up * jumpVeloctiy;
+        }
+
+    }
+
+    private void horizontal()
+    {
+        float currentSpd = moveSpd;
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(moveSpd, rb2d.velocity.y);
+        }
+
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                rb2d.velocity = new Vector2(-moveSpd, rb2d.velocity.y);
+            }
+
+            else
+            {
+                rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            }
+        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D rh2d = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.down, .1f, ground);
+        Debug.Log(rh2d.collider);
+        return rh2d.collider != null;
+    }
+}
+
+
+
+

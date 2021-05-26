5244d8837234c81073a38c9affd42e9e0cc89ded
AwesomeAi <54154904+AwesomeAi@users.noreply.github.com>
2021-05-25 20:50:48 -0500

    Something got done

---
+++ 2Determined/Assets/Scripts/Character stuff/AnimeShuns.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimeShuns : MonoBehaviour
+{
+    Animator animator;
+    SpriteRenderer spr;
+
+    //animation counters
+    int jumpToFall = 0;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+   public void idle(bool isIdle)
+    {
+        if(isIdle)
+        {
+            animator.Play("Judas Animation");
+        }
+    }
+
+    public void direction(string direction)
+    {
+        if (direction.Equals("right"))
+        {
+            spr.flipX = false;
+            
+        }
+        if(direction.Equals("left"))
+        {
+            spr.flipX = true;
+        }
+    }
+
+    public void run()
+    {
+        animator.Play("Judas run");
+    }
+
+    public void jump()
+    {
+        animator.Play("Judas jump");
+    }
+
+    public void falling()
+    {
+        animator.Play("Judas midair");
+    }
+
+    public void wallJump()
+    {
+        animator.Play("jump from wall");
+    }
+
+    public void onWall()
+    {
+        animator.Play("wall");
+    }
+
+    public void fullJump()
+    {
+       if (jumpToFall == 0)
+        {
+            jump();
+            jumpToFall++;
+        }
+       else
+        {
+            falling();
+        }
+    }
+
+    public void jumpReset()
+    {
+        jumpToFall = 0;
+    }
+
+}
--- 2Determined/Assets/Scripts/Character stuff/Movement.cs
+++ 2Determined/Assets/Scripts/Character stuff/Movement.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Movement : MonoBehaviour
 {
+
     //Serialized Fields
     [SerializeField] private LayerMask surface; //checks for anything in the ground layer
 
@@ -14,19 +17,25 @@ public class Movement : MonoBehaviour
     public float airMobility;
 
     //Private variables
+
+    // counts wall jumps to avoid character jumping on wall infinitely
     private int wallJumpsRight = 0;
     private int wallJumpsLeft = 0;
-    // counts wall jumps to avoid character jumping on wall infinitely
+   
+    //animation counters
 
-        
+
+    //creates reference varible for compnenets
     private Rigidbody2D rb2d;
     private BoxCollider2D bc2d;
+    private AnimeShuns a;
 
     // Start is called before the first frame update
     private void Awake()
     {
         rb2d = transform.GetComponent<Rigidbody2D>();
         bc2d = transform.GetComponent<BoxCollider2D>();
+        a = transform.GetComponent<AnimeShuns>();
     }
 
     // Update is called once per frame
@@ -35,7 +44,9 @@ public class Movement : MonoBehaviour
         if(isGrounded()) // checks if player is grounded
         {
             jumping();
+            a.jumpReset();
         }
+        
 
         horizontal();
     }
@@ -47,6 +58,7 @@ public class Movement : MonoBehaviour
         //Checks for keys
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
         {
+            a.fullJump();
             rb2d.velocity = Vector2.up * jumpVeloctiy;
         }
 
@@ -59,23 +71,21 @@ public class Movement : MonoBehaviour
         //moving right
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-           /*
-            if (onWallRight())
-            {
-                jumping();
-            }
-            */
 
             if (isGrounded())
             {
                 rb2d.velocity = new Vector2(moveSpd, rb2d.velocity.y);
+                a.direction("right");
+                a.run();
             }
 
             else
             {
                 rb2d.velocity += new Vector2(moveSpd * airMobility * Time.deltaTime, 0);
                 rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -moveSpd, +moveSpd), rb2d.velocity.y);
+                a.fullJump();
             }
+            
         }
 
         //moving left
@@ -84,12 +94,15 @@ public class Movement : MonoBehaviour
             if (isGrounded())
             {
                 rb2d.velocity = new Vector2(-moveSpd, rb2d.velocity.y);
+                a.direction("left");
+                a.run();
             }
 
             else
             {
                 rb2d.velocity += new Vector2(-moveSpd * airMobility * Time.deltaTime, 0);
                 rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -moveSpd, +moveSpd), rb2d.velocity.y);
+                a.fullJump();
             }
         }
 
@@ -97,6 +110,10 @@ public class Movement : MonoBehaviour
         else if (isGrounded())
         {
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            if(0 == rb2d.velocity.x)
+            {
+                a.idle(true);
+            }
         }
         
     }
@@ -118,7 +135,7 @@ public class Movement : MonoBehaviour
         RaycastHit2D rh2d = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.right, .1f, surface);
         Debug.Log(rh2d.collider);
 
-        if (wallJumpsLeft < 1)
+        if (wallJumpsRight < 1)
         {
             wallJumpsLeft = 0;
             wallJumpsRight += 1;
@@ -133,7 +150,7 @@ public class Movement : MonoBehaviour
         RaycastHit2D rh2d = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.left, .1f, surface);
         Debug.Log(rh2d.collider);
 
-        if (wallJumpsRight < 1)
+        if (wallJumpsLeft < 1)
         {
             wallJumpsRight = 0;
             wallJumpsLeft += 1;
